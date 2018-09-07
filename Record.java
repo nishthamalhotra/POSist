4237699e04cc0121aeb7e5a57ad0f1059b70e66b
nishthamalhotra <36417388+nishthamalhotra@users.noreply.github.com>
2018-09-07 16:28:26 +0530

    Create Record.java

---
+++ Record.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class record{
+    
+    float data;
+    
+    public record(float data){
+        this.data=data;
+    }
+    public static void main(String args[]){
+        
+    }
+}
