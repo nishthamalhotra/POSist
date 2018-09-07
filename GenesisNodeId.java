1a1e10ffa42a6461c4ca7f018d2588ff9d485b37
nishthamalhotra <36417388+nishthamalhotra@users.noreply.github.com>
2018-09-07 16:32:50 +0530

    Create GenesisNodeId.java

---
+++ GenesisNodeId.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class GenesisNode{
+    
+    String GenesisNodeId;
+    
+    public GenesisNode(String GenesisNodeId){
+        this.GenesisNodeId=GenesisNodeId;
+    }
+    
+    public static void main(String[] args){
+        
+    }
+}
